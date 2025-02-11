d6359391fa7f145dd5ff182e1001ced7df818ede
RoiVigo91 <roioliveira71@gmail.com>
2025-02-11 09:08:59 +0100

    Ejercicio30Resuelto

--- src/main/java/com/campusdual/classroom/DivisionByZeroException.java
+++ src/main/java/com/campusdual/classroom/DivisionByZeroException.java
@@ -1,6 +1,6 @@
 package com.campusdual.classroom;
 
-public class DivisionByZeroException extends Throwable {
+public class DivisionByZeroException extends Exception {
     public DivisionByZeroException(String message) {
         super(message);
     }
